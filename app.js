292c779e04b2184cb44ee2ebd072258442df6d12
Zeph Lafassett <zeph@lafassett.com>
2014-08-07 16:02:33 -0700

    adding redis client and connect-redis

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 
 var express = require('express'),
+	bodyParser = require('body-parser'),
 	passport = require('passport'),
 	LocalStrategy = require('passport-local').Strategy,
-	session = require('express-session');
+	session = require('express-session'),
+	redis = require('redis'),
+	RedisStore = require('connect-redis')(session);
 
 
 var app = express();
 
+var users = [
+	{
+		id: 1,
+		username: 'user01',
+		password: 'password'
+	},
+	{
+		id: 2,
+		username: 'user02',
+		password: 'password'
+	}
+];
+// body parser stuff;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
 app.use(session({
 	resave: true,
 	saveUninitialized: true,
-	secret: "keyboard cat!"
+	secret: "keyboard cat!",
+	store: new RedisStore({ prefix: 'test:', host: '127.0.0.1', client: redis.createClient() })
 }));
 
 passport.use('local', new LocalStrategy({
@@ -18,9 +40,23 @@ passport.use('local', new LocalStrategy({
 		passwordField: "password"
 	},
 	function (username, password, done) {
-		return done(null, {id: 12345, username: username});
+		return done(null, {id: 1, username: username});
 	}
 ));
+
+passport.serializeUser(function (user, done) {
+	logger.warn('running serializeUser for user: ' + user.id);
+	done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+	logger.warn('running deserializeUser for id: ' + id);
+	for (var i = 0; i < users.length; i++) {
+		if (user[i].id == id) {
+			done(null, user[i]);
+		}
+	}
+});
 
 app.use(passport.initialize());
 app.use(passport.session(session));
@@ -30,7 +66,7 @@ app.get('/', function (request, response) {
 });
 
 app.get('/login', function (request, response, next) {
-	console.log(request.body);
+
 	passport.authenticate('local', function (err, user, info) {
 		response.json(user);
 	});
